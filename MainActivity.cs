c1c9f23b32f112e51c4936ad77292bc200e7fd39
Moosa Hassan <moosa7021@gmail.com>
2020-09-26 13:41:14 +1000

    Initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace XamarinLearning
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        // attributes
+        TextView txtNumber;
+        int number;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+        }
+    }
+}
---
+++ obj/Debug/90/android/src/crc6418eecb0ec738b790/MainActivity.java
@@ -0,0 +1,47 @@
+package crc6418eecb0ec738b790;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("XamarinLearning.MainActivity, XamarinLearning", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XamarinLearning.MainActivity, XamarinLearning", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
